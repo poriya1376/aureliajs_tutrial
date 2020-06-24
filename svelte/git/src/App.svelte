5462b257fe9a746d7cd0ef3dc3a34b72288730dc
poriya2120 <poriyadaliry2@gmail.com>
2020-06-25 01:45:38 +0430

    sveltejs port one

---
+++ svelte/git/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	
+</main>
+
+<style>
+	
+</style>
